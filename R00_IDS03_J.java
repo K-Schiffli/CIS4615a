bf292fbd7ef8eb7c3f6bd4d5499c543b6b86cff6
K-Schiffli <84538213+K-Schiffli@users.noreply.github.com>
2022-10-17 19:49:27 -0400

    Initial commit

---
+++ R00_IDS03_J.java
@@ -0,0 +1,7 @@
+public class R00_IDS03_J {
+    if(login Successful) {
+        logger.severe ("User login succeeded for: " + username);
+    }else{
+        logger.severe(" User login failed for: " + username);
+    }
+}
