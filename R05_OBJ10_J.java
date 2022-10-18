cd3ab305c711c45b9449613ac28a28b29391bcb1
K-Schiffli <84538213+K-Schiffli@users.noreply.github.com>
2022-10-17 20:23:57 -0400

    Changed serialVersionUID field to Final and Private

--- R05_OBJ10_J.java
+++ R05_OBJ10_J.java
@@ -1,6 +1,5 @@
 public class R05_OBJ10_J {
     class DataSerializer implements Serializable {
-        public static long serialVersionUID = 1973473122623778747L;
-        // ...
+        private static final long serialVersionUID = 1973473122623778747L;
     }
 }
