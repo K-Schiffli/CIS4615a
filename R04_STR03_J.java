e9a83a75ed0889cf60a876de0f33b554097c4070
K-Schiffli <84538213+K-Schiffli@users.noreply.github.com>
2022-10-17 20:11:18 -0400

    Initial commit

---
+++ R04_STR03_J.java
@@ -0,0 +1,8 @@
+public class R04_STR03_J {
+    BigInteger x = new BigInteger("530500452766");
+    byte[]byteArray = x.toByteArray();
+    Strings = new String(byteArray);
+    byteArray = s.getBytes();
+    x = new BigInteger(byteArray);
+}
+
