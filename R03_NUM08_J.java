3e4084d94e6d75d49edda98ab44428d7a78ccb7c
K-Schiffli <84538213+K-Schiffli@users.noreply.github.com>
2022-10-17 20:34:12 -0400

    Added floating point exception checking

--- R03_NUM08_J.java
+++ R03_NUM08_J.java
@@ -1,13 +1,18 @@
 public class R03_NUM08_J {
     double currentBalance; // User's cash balance
-    void doDeposit(String userInput) {
+    void doDeposit(String userInput){
         double val = 0;
         try {
             val = Double.valueOf(userInput);
         } catch (NumberFormatException e) {
             // Handle input format error
         }
-
+        if (Double.isInfinite(val)){
+            // Handle infinity error
+        }
+        if (Double.isNaN(val)) {
+            // Handle NaN error
+        }
         if (val >= Double.MAX_VALUE - currentBalance) {
             // Handle range error
         }
